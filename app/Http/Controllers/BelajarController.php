90ab57457a506d40aa5d09fcb590cbc86b361ec9
idn-fahmifn <fahmifn.idn@gmail.com>
2026-05-24 14:03:20 +0700

    2.3

--- app/Http/Controllers/BelajarController.php
+++ app/Http/Controllers/BelajarController.php
@@ -9,25 +9,7 @@ class BelajarController extends Controller
     // buatkan sebuah fungsi sesuai dengan yang didefinisikan oleh route
     public function proses(Request $request)
     {
-        $request->validate([
-            'nama' => ['required', 'string', 'min:3', 'max:30'],
-            'umur' => ['required', 'integer', 'min:10', 'max:60'],
-        ], [
-
-            // pesan custom saat error
-            // field nama
-            'nama.required' => 'Nama wajib diisi',
-            'nama.string' => 'Nama wajib berbentuk karakter',
-            'nama.min' => 'Nama wajib minimal 3 Karakter',
-            'nama.max' => 'Nama wajib maksimal 30 Karakter',
-
-            //  field umur
-            'umur.required' => 'umur wajib diisi',
-            'umur.integer' => 'umur wajib berbentuk angka',
-            'umur.min' => 'umur wajib minimal 10 tahun',
-            'umur.max' => 'umur wajib maksimal 60 tahun',
-        ]);
-
+        
         // Membuat array berisi data dummy dalam bentuk object
         $daftarMobil = [
             (object) [
